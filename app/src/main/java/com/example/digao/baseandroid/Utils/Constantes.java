588ce2e22ae9185b028ae46b7438d98a991d4625
Digão <digaot.info@gmail.com>
2016-05-07 02:32:46 -0300

    pegando atualização da API

--- app/src/main/java/com/example/digao/baseandroid/Utils/Constantes.java
+++ app/src/main/java/com/example/digao/baseandroid/Utils/Constantes.java
@@ -4,7 +4,7 @@ package com.example.digao.baseandroid.Utils;
  * Created by digao on 04/05/16.
  */
 public class Constantes {
-        public static String urlRoot = "http://192.168.1.5/mmgpApp/www";
+    public static String urlRoot = "http://192.168.1.5/mmgpApp/www";
 //    public static String urlRoot = "http://www.aplicativos.dreamhosters.com/mmgpApp";
     public static String urlConteudo = urlRoot +"/app-conteudo";
     public static String urlCategorias = urlRoot +"/app-categorias";
--- app/src/main/java/com/example/digao/baseandroid/Utils/Functions.java
+++ app/src/main/java/com/example/digao/baseandroid/Utils/Functions.java
@@ -3,6 +3,7 @@ package com.example.digao.baseandroid.Utils;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,10 +12,24 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by digao on 04/05/16.
@@ -24,6 +39,49 @@ public class Functions {
     ;
     public Functions(Context context) {
         mContext = context;
+    }
+
+
+    public void consultaWebservice(String urlConteudo, final String jsonName, final Map<String, String> params){
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlConteudo,
+                new Response.Listener<String>() {
+                    @Override
+                    public String onResponse(String response) {
+                        File file = mContext.getFileStreamPath(jsonName+".json");
+                        if(file.exists()){
+                            try {
+                                JSONObject fileLocal = new JSONObject( read(jsonName) );
+                                JSONObject fileAPI = new JSONObject( response );
+
+
+                                if( !fileLocal.getJSONArray(jsonName).toString().trim().equals( fileAPI.getJSONArray(jsonName).toString().trim() ) ){
+                                    writeFile(fileAPI.toString(), jsonName);
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }else{
+                            writeFile(response, jsonName);
+                        }
+
+                        return response;
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public String onErrorResponse(VolleyError error) {
+                return "erro "+error.getMessage().toString();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams(){
+                return (Map<String, String>) params;
+            }
+        };
+        queue.add(stringRequest);//>>> ele que chama o metodo
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
@@ -45,40 +103,25 @@ public class Functions {
 
 
     public void writeFile(String response, String jsonName){
-        String fileName = jsonName+".json";
         FileOutputStream outputStream = null;
+        File dir = new File(String.valueOf(mContext.getFileStreamPath("app")));
+        String fileName = jsonName+".json";
+
         try {
-            outputStream = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
+            // Create temp file.
+//            File temp = File.createTempFile("TempFileName", ".tmp", mContext.getCacheDir());
+//            BufferedWriter out = new BufferedWriter(new FileWriter(temp));
+//            out.write(response);
+//            out.close();
+
+            if( mContext.getFileStreamPath(fileName).exists() ){
+                mContext.getFileStreamPath(fileName).delete();
+            }
+            outputStream = mContext.openFileOutput( fileName, Context.MODE_PRIVATE);
             outputStream.write(response.getBytes());
             outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            // Handle exceptions here
         }
-    }
-
-
-    public void consultaWebservice(String urlConteudo, final String jsonName, final Map<String, String> params){
-        RequestQueue queue = Volley.newRequestQueue(mContext);
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlConteudo,
-                new Response.Listener<String>() {
-                    @Override
-                    public String onResponse(String response) {
-                        writeFile(response, jsonName);
-
-                        return response;
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public String onErrorResponse(VolleyError error) {
-                return "erro "+error.getMessage().toString();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams(){
-                return (Map<String, String>) params;
-            }
-        };
-        queue.add(stringRequest);//>>> ele que chama o metodo
     }
 }
